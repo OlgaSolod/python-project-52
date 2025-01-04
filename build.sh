32e2345fbb48c1c949e11c6bb737970112cb2fdb
Olga <osoldko@ntv.ru>
2025-01-04 22:05:19 +0300

    upd build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-#pip install -r requirements.txt
-pip install uv
+pip install -r requirements.txt
+# pip install uv
 
-uv sync
+# uv sync
 
 # Convert static asset files
 python manage.py collectstatic --no-input
