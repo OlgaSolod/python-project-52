c3ff7ef58e4b71029232c1e668a0f3c481515454
OlgaSolod <41755983+OlgaSolod@users.noreply.github.com>
2025-01-20 16:19:37 +0300

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,13 +28,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', False)
 if DEBUG:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }
+        'default': dj_database_url.config(
+            default= os.getenv('DATABASE_URl'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
 else:
     DATABASES = {
